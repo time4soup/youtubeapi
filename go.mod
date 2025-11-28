57c12b8b90d1cbad5633ee4f36f83d6f2a2890e6
time4soup <zak.kirk00@gmail.com>
2025-11-28 00:46:47 -0800

    added main function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/time4soup/youtubeapi
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	repl()
+}
+
+func repl() {
+	fmt.Print("running...\n")
+}
